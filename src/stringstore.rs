fd0d18a2ebb575695712d46b357e715885bc027d
Mikko Tanner <mikko.tanner@gmail.com>
2024-07-17 21:41:40 +0300

    stringstore.rs: large rework. Store string hashes instead of pointers in HashMap. StoredStr stores a *const UniqueStrStore instead of a borrow. Add a test.

--- src/stringstore.rs
+++ src/stringstore.rs
@@ -2,7 +2,7 @@
 
 #![allow(dead_code)]
 
-use crate::hashing::CustomXxh3Hasher;
+use crate::hashing::{hash_bytes, CustomXxh3Hasher};
 use size_of::SizeOf;
 use std::{
     cmp::Ordering,
@@ -55,21 +55,30 @@ non-bounds-checking lookup (meant mostly for internal use with known indexes).
 ```
 use statter::stringstore::UniqueStrStore;
 
-let mut store = UniqueStrStore::new();
-assert_eq!(store.len(), 0);
-
 let hello: &'static str = "Hello, world!";
+let mut store = UniqueStrStore::new();
+
 let stored = store.insert(hello);
+assert_eq!(store.len(), 1);
+assert!(store.contains(hello));
+
+let again = store.insert(hello);
+let foo = store.insert("foo");
+assert_eq!(store.len(), 2);
 
 assert_eq!(stored.idx(), 0);
+assert_eq!(again.idx(), 0);
+assert_eq!(foo.idx(), 1);
+
 assert_eq!(stored.as_ref(), hello);
+assert_eq!(stored, again);
 assert_eq!(store.get(0).unwrap(), hello);
-assert_ne!(store.get_unchecked(0), "foo");
+assert_eq!(store.get_unchecked(1), "foo");
 */
 #[derive(Default, Debug, SizeOf)]
 pub struct UniqueStrStore {
     store: Vec<Box<str>>,
-    index: HashMap<*const str, u32, CustomXxh3Hasher>,
+    index: HashMap<u64, u32, CustomXxh3Hasher>,
 }
 
 impl UniqueStrStore {
@@ -77,8 +86,16 @@ impl UniqueStrStore {
         let capacity: usize = 64 * 1024; // 65536 entries to start with
         UniqueStrStore {
             store: Vec::with_capacity(capacity),
-            index: HashMap::with_hasher(CustomXxh3Hasher::default().build_hasher()),
+            index: HashMap::with_capacity_and_hasher(
+                capacity,
+                CustomXxh3Hasher::default().build_hasher(),
+            ),
         }
+    }
+
+    /// Put this [UniqueStrStore] into a [Box].
+    pub fn boxed(self) -> Box<Self> {
+        Box::new(self)
     }
 
     /// The number of unique string slices stored.
@@ -90,41 +107,7 @@ impl UniqueStrStore {
     /// Whether we already have this string slice stored.
     #[inline]
     pub fn contains(&self, s: &str) -> bool {
-        self.index.contains_key(&(s as *const str))
-    }
-
-    /// The reference of a stored string slice, if it exists.
-    #[inline]
-    pub fn get_ref(&self, s: &str) -> Option<StoredStr> {
-        self.index
-            .get(&(s as *const str))
-            .copied()
-            .map(|idx: u32| StoredStr(idx, self))
-    }
-
-    /// Internal function to add a new string slice to the store.
-    fn add_new(&mut self, s: &str) -> StoredStr {
-        let idx: u32 = self.len() as u32;
-        let boxed: Box<str> = s.into();
-        let ptr: *const str = boxed.as_ref();
-        self.index.insert(ptr, idx);
-        self.store.push(boxed);
-        StoredStr(idx, self)
-    }
-
-    /// Insert a new string (slice) and return its [StoredStr] reference.
-    ///
-    /// If the string (slice) already exists, return its reference instead.
-    pub fn insert<T>(&mut self, s: T) -> StoredStr
-    where
-        T: Into<String>,
-    {
-        let s: String = s.into();
-        if self.contains(&s) {
-            self.get_ref(&s).unwrap()
-        } else {
-            self.add_new(&s)
-        }
+        self.index.contains_key(&hash_bytes(s.as_bytes()))
     }
 
     /// Get a reference to a stored string slice by its index, if it exists.
@@ -145,20 +128,47 @@ impl UniqueStrStore {
         unsafe { self.store.get_unchecked(idx as usize).as_ref() }
     }
 
-    /// Get a stored string slice by its reference.
-    pub fn from_ref<'a>(&'a self, r: StoredStr) -> &'a str {
-        self.get_unchecked(r.into())
+    #[inline]
+    fn as_ptr(&self) -> *const UniqueStrStore {
+        self as *const UniqueStrStore
+    }
+
+    /// The reference of a stored string slice, if it exists.
+    #[inline]
+    pub fn get_ref(&self, s: &str) -> Option<StoredStr> {
+        self.index
+            .get(&hash_bytes(s.as_bytes()))
+            .copied()
+            .map(|idx: u32| StoredStr(idx, self.as_ptr()))
+    }
+
+    /// Insert a new string (slice) and return its [StoredStr] reference.
+    ///
+    /// If the string (slice) already exists, return its reference instead.
+    pub fn insert<T>(&mut self, s: T) -> StoredStr
+    where
+        T: Into<String>,
+    {
+        let s: String = s.into();
+        if self.contains(&s) {
+            self.get_ref(&s).unwrap()
+        } else {
+            let idx: u32 = self.len() as u32;
+            self.index.insert(hash_bytes(s.as_bytes()), idx);
+            self.store.push(s.into());
+            StoredStr(idx, self.as_ptr())
+        }
     }
 }
 
 /// A reference (index) to a stored string slice in a [UniqueStrStore].
 #[derive(Clone)]
-pub struct StoredStr<'a>(u32, &'a UniqueStrStore);
+pub struct StoredStr(u32, *const UniqueStrStore);
 
-impl StoredStr<'_> {
+impl StoredStr {
     #[inline]
     fn reference(&self) -> &str {
-        self.1.get_unchecked(self.0)
+        unsafe { (*self.1).get_unchecked(self.0) }
     }
 
     /// Get the index of the stored string slice.
@@ -169,7 +179,7 @@ impl StoredStr<'_> {
 
     /// Get the reference to the [UniqueStrStore] that contains this string.
     pub fn store(&self) -> &UniqueStrStore {
-        self.1
+        unsafe { &*self.1 }
     }
 
     pub fn cloned(&self) -> String {
@@ -179,13 +189,13 @@ impl StoredStr<'_> {
 
 /* --------------------------------- */
 
-impl AsRef<str> for StoredStr<'_> {
+impl AsRef<str> for StoredStr {
     fn as_ref(&self) -> &str {
         self.reference()
     }
 }
 
-impl Deref for StoredStr<'_> {
+impl Deref for StoredStr {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
@@ -195,27 +205,27 @@ impl Deref for StoredStr<'_> {
 
 /* --------------------------------- */
 
-impl Eq for StoredStr<'_> {}
+impl Eq for StoredStr {}
 
-impl PartialEq for StoredStr<'_> {
+impl PartialEq for StoredStr {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0
     }
 }
 
-impl PartialOrd for StoredStr<'_> {
+impl PartialOrd for StoredStr {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.0.partial_cmp(&other.0)
     }
 }
 
-impl Ord for StoredStr<'_> {
+impl Ord for StoredStr {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.cmp(&other.0)
     }
 }
 
-impl Hash for StoredStr<'_> {
+impl Hash for StoredStr {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.0.hash(state)
     }
@@ -223,13 +233,13 @@ impl Hash for StoredStr<'_> {
 
 /* --------------------------------- */
 
-impl Debug for StoredStr<'_> {
+impl Debug for StoredStr {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "StoredStr({}: {})", self.idx(), self.reference())
     }
 }
 
-impl Display for StoredStr<'_> {
+impl Display for StoredStr {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.reference())
     }
@@ -238,14 +248,44 @@ impl Display for StoredStr<'_> {
 /* --------------------------------- */
 
 // Implement `From` for converting `StoredStr` into `u32`.
-impl<'a> From<StoredStr<'a>> for u32 {
+impl<'a> From<StoredStr> for u32 {
     fn from(v: StoredStr) -> u32 {
         v.0
     }
 }
 
-impl<'a> From<StoredStr<'a>> for &'a str {
-    fn from(v: StoredStr<'a>) -> &'a str {
-        v.1.get_unchecked(v.0)
+impl<'a> From<StoredStr> for &'a str {
+    fn from(v: StoredStr) -> &'a str {
+        unsafe { (*v.1).get_unchecked(v.0) }
+    }
+}
+
+/* ######################################################################### */
+
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test_unique_store_basic() {
+        let hello: &'static str = "Hello, world!";
+        let mut store = UniqueStrStore::new();
+
+        let stored = store.insert(hello);
+        assert_eq!(store.len(), 1, "Store length should be 1");
+        assert!(store.contains(hello), "Store does not contain 'Hello, world!': {store:?}");
+
+        let again = store.insert(hello);
+        let foo = store.insert("foo");
+        assert_eq!(store.len(), 2, "Store length should be 2");
+
+        assert_eq!(stored.idx(), 0, "'Hello, world!' index should be 0: {stored:?}");
+        assert_eq!(again.idx(), 0, "Second 'Hello, world!' index should be again 0: {again:?}");
+        assert_eq!(foo.idx(), 1, "'foo' index should be 1: {foo:?}");
+
+        assert_eq!(stored.as_ref(), hello, "as_ref() should == 'Hello, world!': {stored:?}");
+        assert_eq!(stored, again, "StoredStr instances should be equal: {stored:?} != {again:?}");
+        assert_eq!(store.get(0).unwrap(), hello, "get(0) should == 'Hello, world!'");
+        assert_eq!(store.get_unchecked(1), "foo", "get_unchecked(1) should == 'foo'");
     }
 }
